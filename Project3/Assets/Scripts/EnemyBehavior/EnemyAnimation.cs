2e333f727f332010478e991abdabbcb9b9a94f93
hienlq161 <hien.lequang@hcmut.edu.vn>
2023-05-12 21:15:10 +0700

    Remove unoptimized variable

--- Project3/Assets/Scripts/EnemyBehavior/EnemyAnimation.cs
+++ Project3/Assets/Scripts/EnemyBehavior/EnemyAnimation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAnimation : MonoBehaviour{
-    [SerializeField] float attackCoolDown = 1f;
     
     [SerializeField] Animator animator;
     [SerializeField] EnemyAI enemyAI;
